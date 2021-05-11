9df9b021895d5736334616ad4c83e7aba52bc492
Wayne Robinson <l00162024@student.lyit.ie>
2021-05-11 15:06:26 +0100

    Issue with welcome message

--- react-frontend-ems/src/components/ErrorComponent.jsx
+++ react-frontend-ems/src/components/ErrorComponent.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 class ErrorComponent extends Component{
+
     render(){
         return <h2>An Error Occurred! Please contact the following emial address: L00162024@Student.lyit.ie</h2>  
    }
