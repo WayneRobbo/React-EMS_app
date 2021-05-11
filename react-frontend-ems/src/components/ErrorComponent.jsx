1e6d621119791d8e1744b198cd0301194bde4f43
Wayne Robinson <l00162024@student.lyit.ie>
2021-05-11 22:51:19 +0100

    addition of logout page and updated navigation bar

--- react-frontend-ems/src/components/ErrorComponent.jsx
+++ react-frontend-ems/src/components/ErrorComponent.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ErrorComponent extends Component{
 
     render(){
-        return <h2>An Error Occurred! Please contact the following emial address: L00162024@Student.lyit.ie</h2>  
+        return <h2>An Error Occurred! Please contact the following email address: L00162024@student.lyit.ie</h2>  
    }
 }
 
