42719c086fbbe72d95f51eacc41747b618acf3fa
Wayne Robinson <l00162024@student.lyit.ie>
2021-05-13 16:14:40 +0100

    route auth working

--- react-frontend-ems/src/components/AuthenticationService.js
+++ react-frontend-ems/src/components/AuthenticationService.js
@@ -1,11 +1,10 @@
 class AuthenticationService {
+
     registerSuccesfulLogin(username,password){
-        console.log("succesful login");
         sessionStorage.setItem('authenticatedUser', username);
     }
 
     logoutSuccessful(username, password){
-        console.log("succeful logout");
         sessionStorage.removeItem('authenticatedUser',username);
     }
 
