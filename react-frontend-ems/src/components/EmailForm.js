ef086a86f8eb5cc8834ae470626a32935d9286eb
Wayne Robinson <l00162024@student.lyit.ie>
2021-05-21 11:50:57 +0100

    forgot password email

---
+++ react-frontend-ems/src/components/EmailForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import emailjs from "emailjs-com";
+
+export default function ContactUs() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('gmail', 'template_y81omic', e.target, 'user_ymWC16QG0r318Q0EBpdGS')
+          .then((result) => {
+              console.log(result.text);
+              console.log("email");
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      }
+  
+    return (
+            <form className="contact-form" onSubmit={sendEmail}>
+                <div>
+                    <th> First Name: </th>
+                    <input placeholder = "First Name" name = "firstName" className = "form-control"/>
+            
+                    <th> Last Name: </th>
+                    <input placeholder = "Last Name" name = "lastName" className = "form-control"/>
+
+                    <th> Email </th>
+                    <input placeholder = "Email Id" name = "emailId" className = "form-control"/>
+
+                    <th> Date </th>
+                    <input placeholder = "Shift Date" name = "date" className = "form-control"/>
+                    
+                </div>
+                <input type="submit" value="Send" />
+            </form>
+    );
+  }
